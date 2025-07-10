7b8a02ae8dd222585b21e9193579f2eb6c201092
Patrik Dóczy <doczy.patrik410@gmail.com>
2025-07-10 10:10:17 +0200

    lazy bootstrap

--- init.lua
+++ init.lua
@@ -1,8 +1,23 @@
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 vim.opt.termguicolors = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 require("config")
+require("lazy").setup("plugins")
 require("keymaps")
 
 vim.opt.relativenumber = true
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
+
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "bash-language-server",        -- bashls
+          "css-lsp",                     -- cssls
+          "dockerfile-language-server",  -- dockerls
+          "eslint-lsp",                  -- eslint
+          "gdtoolkit",
+          "html-lsp",                    -- html
+          "json-lsp",                    -- jsonls
+          "lua-language-server",         -- lua_ls
+          "marksman",
+          "prettierd",
+          "pyright",
+          "trivy",
+          "typescript-language-server",  -- ts_ls
+          "typos-lsp",                   -- typos_lsp
+          "yaml-language-server",        -- yamlls
+        },
+      })
+    end,
+  },
+
+  {
+    "WhoIsSethDaniel/mason-tool-installer",
+    dependencies = { "williamboman/mason.nvim" },
+  },
+}
